46248ad8e0c6a7a47910aa30dd950a9ccfddfe75
RahulGajare <rahulgajare0>
2019-02-12 10:12:43 +0530

    added Header

--- StopWatchProgram.cs
+++ StopWatchProgram.cs
@@ -1,12 +1,26 @@
-﻿namespace FunctionalPrograms
+﻿// --------------------------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="StopWatchProgram.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator name="Rahul Gajare"/>
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace FunctionalPrograms
 {
     using System;
     using System.Collections.Generic;
-    using System.Text;
     using System.Diagnostics;
+    using System.Text;
     using System.Threading;
+
+    /// <summary>
+    /// Simulates a stopwatch
+    /// </summary>
     public class StopWatchProgram
     {
+        /// <summary>
+        /// Gives the elapsed time between start and stop of stopwatch
+        /// </summary>
         public static void StartStopWatch()
         {
             Stopwatch stopWatch = new Stopwatch();
@@ -15,6 +29,5 @@
             Console.ReadKey();
             Console.WriteLine("Total Elapsed time = " + stopWatch.Elapsed);
         }
-
     }
 }
